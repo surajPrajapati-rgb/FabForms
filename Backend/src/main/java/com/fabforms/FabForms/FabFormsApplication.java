221ab65a349b10036f0642495078f35b506771d5
surajPrajapati-rgb <jnvsuraj7388@gmail.com>
2025-02-02 08:00:21 +0530

    initial commit

---
+++ Backend/src/main/java/com/fabforms/FabForms/FabFormsApplication.java
@@ -0,0 +1,13 @@
+package com.fabforms.FabForms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabFormsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabFormsApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/fabforms/FabForms/FabFormsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabforms.FabForms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabFormsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
